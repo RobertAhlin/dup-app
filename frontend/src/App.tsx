70382c97aeffc8d5d1621255d3842cb57983bcee
Robert Ahlin <rob@ahlin.org>
2025-11-27 16:14:37 +0100

    Configure for deploy. Change hero image.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,24 @@
-import logo from './assets/hero-image.png'
+import logo from './assets/dup-logo-colors-web_01.png'
 
 function App() {
 
   return (
     <>
       <div>
-        <a href="http://localhost:5173/login">
-          <img src={logo} className="logo DUP" alt="DUP logo" />
+        <a href="/login">
+          <img src={logo} className="logo DUP max-h-100" alt="DUP logo" />
         </a>
+        <div className="w-full flex justify-center items-center mt-4">
+          <h1 className="text-4xl font-bold text-center">
+            Digital Upskill Platform
+          </h1>
+          
+        </div>
+        <div className="w-full flex justify-center items-center text-gray-600">
+        <p>
+          (Click logo to login)
+          </p>
+        </div>
       </div>
 
     </>
